d2f3a563f3160c33bb870206d601938ec4a8e564
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2025-01-13 18:45:02 -0500

    test

--- TurboModeActions/TurboActions.cs
+++ TurboModeActions/TurboActions.cs
@@ -8,7 +8,7 @@ namespace TurboActions
     {
         internal const string Guid = "omegaplatinum.elin.turboactions";
         internal const string Name = "Turbo Actions";
-        internal const string Version = "1.1.3.1";
+        internal const string Version = "1.1.3.2";
     }
 
     [BepInPlugin(GUID: ModInfo.Guid, Name: ModInfo.Name, Version: ModInfo.Version)]
